e4ba3a8a82c759133392201657770d40e8ce3e44
Parth Patel <parthpatel317@gmail.com>
2017-01-25 14:28:33 -0800

    add questions from release 0

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What's your name?"
+	name = gets.chomp
+
+puts "How old are you? What year were you born?"
+	ageyear = gets.chomp 
+
+puts "Our cafeteria serves garlic bread. Should we order some for you?"
+	bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+	health = gets.chomp
