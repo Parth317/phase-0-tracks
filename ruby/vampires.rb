01115428a900fa26ed5b7b042e536b7626909f53
Parth Patel <parthpatel317@gmail.com>
2017-01-27 16:57:16 -0800

    add part of release 2 to sublime

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,47 @@
+
+
 puts "What's your name?"
 	name = gets.chomp
 
-puts "How old are you? What year were you born?"
-	ageyear = gets.chomp 
+puts "How old are you?" 
+	age = gets.chomp 
+
+puts "What year were you born?"	
+	year = gets.chomp
 
 puts "Our cafeteria serves garlic bread. Should we order some for you?"
 	bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?"
 	health = gets.chomp
+
+	if ageyear && bread 
+		puts "Your age is correct and here's garlic bread!"
+	else
+		puts "You're wrong. No garlic bread for you!"
+				
+		
+	end
+
+	if (2017-year) == age
+		puts "Correct age"
+		ageright = true
+	else ageright = false	
+		
+	end
+
+	if bread == yes
+		puts "wants garlic bread"
+		garlicbread = true
+
+	else garlicbread = false
+				
+		
+	end
+
+	if ageright && garlicbread 
+		puts "probably not a vampire"
+		
+	end
+
+	
