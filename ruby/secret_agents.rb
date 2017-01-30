8a8e47c32670909bc24309ac61f355d03d8079c4
Parth Patel <parthpatel317@gmail.com>
2017-01-29 16:04:26 -0800

    fix spacing and indentations in code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -2,7 +2,6 @@ def encrypt (string)
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
 	count = 0
 	#move every letter of a string one letter forward (abcd becomes bcde)
-	#length of string is 4
 	string_2 = ' '
 	while count < string.length
 		alpha_list = alphabet[alphabet.index(string[count]) + 1]
@@ -10,48 +9,34 @@ def encrypt (string)
 		count += 1
 		
 end
-puts string_2
+	puts string_2
 end
-
-#encrypt "abc"
-
-#string = 'fbac'
-#alpha = 'abcdefghi...'
-
-
-#count = 0 
-#puts string[count] 
-#alpha[]
-#string[count] = alpha[alpha.index(f) -1] 
 
 
 def decrypt (string)
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-  count = 0
-  #length of string is 4
+  	alphabet = "abcdefghijklmnopqrstuvwxyz"
+  	count = 0
     new_string = ' '
-  while count < string.length
+ 	while count < string.length
  #move every letter of a string back one letter (bcde becomes abcd)
  #find out where letter is in the index
  #access the letter right before it
  
-    
-      
-      new_char = alphabet[alphabet.index(string[count]) - 1]   
-      new_string += new_char
-    count += 1
-  end
+      	new_char = alphabet[alphabet.index(string[count]) - 1]   
+     	new_string += new_char
+    	count += 1
+ end
     puts new_string
 end
 #decrypt "bcd"
 #decrypt(encrypt("swordfish"))
 #this nested method call works because it calls the outside method first, then calls the inside method. The encypt will be added to the decrypt.
 
-puts "Would you like to decrypt or encrypt a password?"
-	answer = gets.chomp
+	puts "Would you like to decrypt or encrypt a password?"
+		answer = gets.chomp
 			
-puts "Please enter your password:"
-	password = gets.chomp	
+	puts "Please enter your password:"
+		password = gets.chomp	
 
 	if answer == "encrypt"
 		puts encrypt(password)
