bd4773578065146a8aeb34bb8f4aaa70be08e334
Parth Patel <parthpatel317@gmail.com>
2017-01-31 11:21:38 -0800

    add methods to lines

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -19,21 +19,21 @@
  "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+ " suspects".insert(0, "The usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+ "The case of the disappearing last letter".chomp("r")
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+ "The mystery of the missing first letter".slice(0)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+ "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+ "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+ "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
