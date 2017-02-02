eefa65f319b51bad099758925dded10a8005c57a
Parth Patel <parthpatel317@gmail.com>
2017-02-01 19:10:55 -0800

    add hash info

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,5 @@
+#print the questions to gather information about the user/clients
+#user input and use .to_i for any integer related questions
 puts "Please enter your information!"
 puts "What's your client's name?"
 	name = gets.chomp
@@ -8,11 +10,15 @@ puts "How many children do you have?"
 puts "What's your decor theme going to be?"	
 	theme = gets.chomp
 
+#create a hash and a key
 info = {
-	"name" => name
-	"age" => age
+	:age => age
+
 }
 
+#print the hash and then let the user change a key
 p info
-#info["name"] = gets.chomp
-
+info["name"] = gets.chomp
+puts "change any info"
+info["age"] = gets.chomp.to_i
+p info
