7aa15b74d36ed49b1f5649263ac322a5d5da63f0
Parth Patel <parthpatel317@gmail.com>
2017-02-03 15:22:56 -0800

    add fibonacci

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,6 +1,26 @@
-numbers = ["24", "28", "335", "42"]
+#
+#numbers = ["24", "28", "335", "42"]
+#
+#p numbers.each_index.select{|i| numbers[i] == "42"}
 
-p numbers.each_index.select{|i| numbers[i] == "42"}
+def fibonacci(n)
+  a = 0
+  b = 1
+  n.times do
+  store = a
+  a = b
+  b = store + b
+  end
+  return a
+end
+
+arr = []
+
+ 6.times do |n|
+  result = fibonacci(n)
+ arr.push(result)
+ p arr
+end
 
 
 
