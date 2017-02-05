a47426d6979458334ee6d22e3cffc69db0850ca3
Parth Patel <parthpatel317@gmail.com>
2017-02-05 10:54:22 -0800

    add more nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,7 +1,6 @@
 #put a bunch of arrays in a hash, then print everything.
 
 Dealership = {
-	'Locations' => ['NYC', 'SF', 'LA', 'DC']
 	'Service advisors' => ['Don', 'Parth', 'Derek', 'Kuan']
 	Car_make: {
 		bmw: '335',
@@ -9,8 +8,16 @@ Dealership = {
 			cars_avail: 2000,
 			cars_sold: 203948
 		}
+		used_inventory_makes: ['Acura', 'Lexus', 'Porsche', 'Audi']
+	}
+
+	Locations: {
+		cities: ['NYC', 'SF', 'LA', 'DC']
+		how_many_locations: {
+			num_of_locations: 4
+		}
 	}
 
 
-
 }
+
