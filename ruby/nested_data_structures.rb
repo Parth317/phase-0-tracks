9790c544ac5a576bd84d9e046d94b0fd4e4730d8
Parth Patel <parthpatel317@gmail.com>
2017-02-05 15:12:41 -0800

    print specific items that are nested

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,8 +1,8 @@
 #put a bunch of arrays in a hash, then print everything.
 
-Dealership = {
-	#'Service advisors' => ['Don', 'Parth', 'Derek', 'Kuan']
-	Car_make: {
+dealership = {
+	'Service advisors' => ['Don', 'Parth', 'Derek', 'Kuan'],
+	car_make: {
 		bmw: '335',
 		inventory: {
 			cars_avail: 2000,
@@ -13,7 +13,7 @@ Dealership = {
 	},
 
 	Locations: {
-		cities: ['NYC', 'SF', 'LA', 'DC'],
+		:cities => ['NYC', 'SF', 'LA', 'DC'],
 		how_many_locations: {
 			num_of_locations: 4
 		},
@@ -21,3 +21,8 @@ Dealership = {
 
 
 }
+
+p dealership['Service advisors'][1]
+p dealership[:Locations][:how_many_locations]
+p dealership[:Locations][:cities]
+p dealership[:car_make][:cars_avail]
