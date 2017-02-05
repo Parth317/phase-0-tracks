84d7c490f991f3358992c031e0965bbb85ffaeae
Parth Patel <parthpatel317@gmail.com>
2017-02-05 10:21:59 -0800

    add arrays and hashes

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,16 @@
+#put a bunch of arrays in a hash, then print everything.
+
+Dealership = {
+	'Locations' => ['NYC', 'SF', 'LA', 'DC']
+	'Service advisors' => ['Don', 'Parth', 'Derek', 'Kuan']
+	Car_make: {
+		bmw: '335',
+		inventory: {
+			cars_avail: 2000,
+			cars_sold: 203948
+		}
+	}
+
+
+
+}
