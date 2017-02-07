bd518dc93613c58802c980a5a8703c3713734d42
Parth Patel <parthpatel317@gmail.com>
2017-02-07 14:37:40 -0800

    add release 0 to puppymethods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,31 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+  	p "Woof! " * integer
+  end
+
+  def roll_over
+  	p "rolls over"
+  end
+
+  def dog_years(human_years)
+  	p 7 * human_years
+  end
+
+  def rip_paper
+  	p "rips newspaper"
+  end
+
 end
 
+a = Puppy.new
+a.fetch("toy")
+
+a.speak(3)
+
+a.roll_over
+
+a.dog_years(2)
+
+a.rip_paper
