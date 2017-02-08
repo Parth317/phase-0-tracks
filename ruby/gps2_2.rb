a034a593aa05d81eecd31cda8cbfdff91b960ce7
Parth Patel <parthpatel317@gmail.com>
2017-02-07 20:03:09 -0800

    create a new list, adding new items, and removing one

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,3 +1,4 @@
+#create a grocery list method and start out with each item quantity = 1
 
 def create_list(groceries)
 	grocery_array = groceries.split
@@ -8,36 +9,51 @@ return grocery_list
 
 end
 
-
+#add an item of your choice to the existing list
 def add_item(new_item, existing_list)
 	existing_list[new_item] = 1
-return existing_list
-	
+	return existing_list
+
 end
 
+#remove an item of your choice from the existing list
 def remove_item(new_item, existing_list)
 	existing_list.delete(new_item)
 	 existing_list
 	
 end
 
+#update the quantity of one or more items in your list
 def update_quantity(new_item, existing_list, quantity)
 	existing_list[new_item] = quantity
 	existing_list
 	
 end
 
+#make the list look cool 
 def pretty_list(groceries)
 	puts "Grocery List"
 	puts
 	groceries.each do |new_item, quantity|
-		puts "#{new_item.to_s.capitalize} : #{quantity}"
+	puts "#{new_item.to_s.capitalize} : #{quantity}"
+	puts
 	end
 	
 end
 
-p my_list = create_list("carrots apples cereal pizza")
-p add_item("tacos", my_list)
-p remove_item("carrots", my_list)
-p update_quantity("tacos", my_list, 5)
+#print all of the methods
+ my_list = create_list("carrots apples cereal pizza")
+ add_item("tacos", my_list)
+ remove_item("carrots", my_list)
+ update_quantity("tacos", my_list, 5)
 p pretty_list(my_list)
+
+
+ new_list = create_list("lemonade tomatoes onions")
+ add_item("ice cream", new_list)
+ update_quantity("lemonade", new_list, 2)
+ update_quantity("tomatoes", new_list, 3)
+ update_quantity("ice cream", new_list, 4)
+ remove_item("lemonade", new_list)
+ update_quantity("ice cream", new_list, 1)
+p pretty_list(new_list)
