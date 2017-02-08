3e6830c67ff96e2853e1074cf99b43602c42209e
Parth Patel <parthpatel317@gmail.com>
2017-02-07 19:38:41 -0800

    make list look pretty

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -21,10 +21,23 @@ def remove_item(new_item, existing_list)
 	
 end
 
-def update_num
+def update_quantity(new_item, existing_list, quantity)
+	existing_list[new_item] = quantity
+	existing_list
+	
+end
+
+def pretty_list(groceries)
+	puts "Grocery List"
+	puts
+	groceries.each do |new_item, quantity|
+		puts "#{new_item.to_s.capitalize} : #{quantity}"
+	end
 	
 end
 
 p my_list = create_list("carrots apples cereal pizza")
 p add_item("tacos", my_list)
 p remove_item("carrots", my_list)
+p update_quantity("tacos", my_list, 5)
+p pretty_list(my_list)
