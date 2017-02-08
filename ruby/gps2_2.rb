b31455b838a262c71bf3c12d1e05e3209ec47c19
Parth Patel <parthpatel317@gmail.com>
2017-02-07 18:51:58 -0800

    add method to add an item to a list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -15,6 +15,16 @@ return existing_list
 	
 end
 
+def remove_item(new_item, existing_list)
+	existing_list.delete(new_item)
+	 existing_list
+	
+end
+
+def update_num
+	
+end
 
 p my_list = create_list("carrots apples cereal pizza")
 p add_item("tacos", my_list)
+p remove_item("carrots", my_list)
