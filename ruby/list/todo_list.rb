b1595232dd73d756f93d386dce23e0ca66fc9e8a
Parth Patel <parthpatel317@gmail.com>
2017-02-09 12:30:45 -0800

    add methods to todo_list and finish

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,9 +1,22 @@
 class TodoList
-	def initialize(list_items)
-		list.get_items = ["do the dishes", "mow the lawn"]
+    
+    def initialize(list)
+        @list = list
+    end
 
-		return list_items
-		
-	end
+    def get_items
+        @list
+    end
 
+    def add_item(item)
+        @list << item
+    end
+
+    def delete_item(item)
+        @list.delete(item)
+    end
+    
+    def get_item(index)
+        @list[index]
+    end
 end
