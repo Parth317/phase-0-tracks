a79177eddfabdbeb9acc729adb0a6e07572a83fb
Parth Patel <parthpatel317@gmail.com>
2017-02-09 19:17:24 -0800

    add some cool initialization as driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,5 @@
+#make a santa class, with methods to go along with it
+#also create an initialize method that will do many things
 class Santa
 	def speak(holidays)
 		p "Ho, ho, ho! Haaaapy #{holidays}!"
@@ -18,6 +20,7 @@ class Santa
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer",
 				 "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
+		
 	end
 
 	def reindeer_name
@@ -40,3 +43,13 @@ a.eat_milk_and_cookies("choco chip cookie")
 
 a.reindeer_name
 a.santa_age
+
+#add some diverse initializations
+#have it print santa is overweight and underweight
+#also have it print that he's always and never eating cookies
+santas = []
+weight = ["little overweight", "little underweight"]
+eating_cookies = ["always eating cookies", "never eating cookies"]
+weight.length.times do |i|
+	santas << Santa.new(weight[i], eating_cookies[i])
+end
