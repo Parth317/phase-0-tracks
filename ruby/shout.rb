f5c7770fbe3feaf82a957ce679bb38e3c71ac3c5
Parth Patel <parthpatel317@gmail.com>
2017-02-11 17:40:41 -0800

    add mixin version of the shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   def self.yell_angrily(words)
   	words + "!!!" + " :("
@@ -11,3 +12,30 @@ end
 #DRIVER CODE
 p Shout.yell_angrily("HEY YOU")
 p Shout.yelling_happily("HI I'M HERE")
+=end
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+  	end
+
+  	def yelling_happily(words)
+  		words + "!!!" + " :D"
+  	end
+  end
+
+  class Neighbor
+  	include Shout
+  end
+
+  class Teacher
+  	include Shout
+  end
+
+ #DRIVER CODE
+
+ neighbor = Neighbor.new
+ p neighbor.yell_angrily("I HATE YOU")
+
+ teacher = Teacher.new
+ p teacher.yelling_happily("GOOD JOB")
