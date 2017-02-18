45fd204dfd3c6531dfbe217948433375dec51817
Parth Patel <parthpatel317@gmail.com>
2017-02-18 15:09:43 -0800

    start a loop with a blank hash, to add the keys

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,21 @@ var names = ["Ed", "Ben", "Frank", "Larry"];
 
 colors.push("Orange");
 names.push("Joe");
+
+
+
+
+//put each element of names into a key of a hash
+//each element of colors into a value of the same hash
+//element.length can vary
+
+// var horses = {
+// 	horse_name: ""
+// }
+
+for (var i = 0; i < colors.length; i++) {
+	var horses = {}
+	horses["horse_name"] = colors[i];
+}
+
+console.log(horses)
