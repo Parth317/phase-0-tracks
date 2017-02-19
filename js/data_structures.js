134ff7c1d408b58fb7e4b596fc48717e7035e003
Parth Patel <parthpatel317@gmail.com>
2017-02-18 21:24:48 -0800

    add racecar constructor function with driver code at the bottom

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,4 @@
+//Code and arrays for the first function. Lines 2-19
 var colors = ["blue", "red", "green", "yellow"];
 var names = ["Ed", "Ben", "Frank", "Larry"];
 
@@ -17,4 +18,21 @@ for (var i = 0; i < names.length; i++) {
 	horses[names[i]] = colors[i]
 }
 
+//Start the constructor function for a car here
+function Race_Car(power, torque){
+
+//add the javascript version of an instance variable here
+this.power = power;
+this.torque = torque;
+this.car_make = function() {
+	console.log("BMW"); 
+	} 
+
+}
+
+//DRIVER CODE
 console.log(horses)
+//
+var new_race_car = new Race_Car(335, 400);
+console.log(new_race_car);
+new_race_car.car_make();
