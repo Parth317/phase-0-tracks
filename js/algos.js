662ae6869a96a849024c37b05dcf8a9736d4b688
Parth Patel <parthpatel317@gmail.com>
2017-02-20 16:33:52 -0800

    create two hashes and compare the keys/values

--- js/algos.js
+++ js/algos.js
@@ -13,3 +13,28 @@ for (var i = 0; i < food.length; i++) {
 	}
 }
 console.log(longest_string);
+
+//create two hashes and check to see if they have common keys/values
+var names = ["Joe", "Jim", "Bob"];
+var location = ["India", "Canada", "LA"];
+location.push("Ohio");
+hashone = {}
+hashtwo = {}
+
+for (var i = 0; i <names.length; i++) {
+		hashone[names[i]] = location[i]
+		hashtwo[names[i]] = location[i]
+	}
+
+function compare(hashone, hashtwo){
+	return JSON.stringify(hashone) === JSON.stringify(hashtwo);
+}
+
+compare(hashone, hashtwo);
+console.log(hashone, hashtwo)
+
+//function that takes an integer for length
+function integers(length) {
+	
+}
+
