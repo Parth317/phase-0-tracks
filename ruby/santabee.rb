06e35add36c6e3c4f9178d612957bae5214c3560
Parth Patel <parthpatel317@gmail.com>
2017-02-28 19:02:20 -0800

    add diverse initializations to santadriver code

--- ruby/santabee.rb
+++ ruby/santabee.rb
@@ -24,3 +24,11 @@ end
 	p santyl = Santa.new("male", "alien")
 	santyl.speak("holidays")
 	santyl.eat_milk_and_cookies("choco chip")
+
+	#add driverse initializations
+	santas = []
+	example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
