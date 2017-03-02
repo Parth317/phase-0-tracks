882b46047434d2397709b87000af10ac36396238
Parth Patel <parthpatel317@gmail.com>
2017-03-01 23:54:17 -0800

    add mixin version of shout module with two classes, then driver code to follow

--- ruby/shoutbee.rb
+++ ruby/shoutbee.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
 end
@@ -7,7 +8,7 @@ module Shout
     words + "!!!" + " :("
   end
 end
-
+	#add a yell happily method here
 module Shout
 	def self.yell_happily(words)
 		words + "!!" + " :)"
@@ -16,4 +17,31 @@ module Shout
 #DRIVER CODE
 p Shout.yell_angrily("I HATE YOU")
 p Shout.yell_happily("I LOVE YOU")
+end
+=end
+
+#add a mixin version of the Shout module with two classes
+module Shout
+
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		words + "!!" + " :("
+	end
+
+	class Toddler
+		include Shout
+	end	
+
+	class Old_man
+		include Shout
+	end	
+
+	#DRIVER CODE(again)
+toddler = Toddler.new
+p toddler.yell_happily("I LIKE ICECREAM")
+old_man = Old_man.new
+p old_man.yell_angrily("GET OFF MY LAWN, PUNK")
 end
