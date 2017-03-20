b1b66d8ce755d1e40e3b2bf5baaac851c2ac78b5
Parth Patel <parthpatel317@gmail.com>
2017-03-19 23:55:43 -0700

    add a string delimiter

--- databases/awesome_build/furniture.rb
+++ databases/awesome_build/furniture.rb
@@ -1,3 +1,16 @@
 #Furniture Shopping
 
+#require gems
 require 'sqlite3'
+
+#create an SQLite3 database
+db = SQLite3::Database.new("furniture.db")
+db.results_as_hash = true
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS kittens(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    quantity INT
+  )
+SQL
