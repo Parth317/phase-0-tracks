afc2184d9a27f2e157727d5533eba4e105254e69
Parth Patel <parthpatel317@gmail.com>
2017-03-20 00:40:55 -0700

    add methods to remove furniture items. try to get program to run

--- databases/awesome_build/furniture.rb
+++ databases/awesome_build/furniture.rb
@@ -27,3 +27,21 @@ quantity = gets.chomp.to_i
 def add_furniture(db, item, quantity)
 	db.execute("INSERT INTO furniture (item, quantity) VALUES (?, ?)" [item, quantity])
 end
+
+add_furniture(db, "#{item}", "#{quantity}")
+
+puts "Enter bought items to be removed from list, or type 'skip'."
+bought = gets.chomp 
+
+def remove_furniture(db, bought)
+  db.execute("DELETE FROM furniture WHERE item='#{bought}'")
+end
+
+if !bought == "skip"
+  remove_item(db, "#{bought}")
+else 
+  list = db.execute("SELECT * FROM furniture")
+  list.each do |buy|
+  puts "#{buy['item']}: #{buy['quantity']}"
+  end
+end
