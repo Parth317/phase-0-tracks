bdd86de11b47fce76e583e6e89a1eab659ff5ff8
Parth Patel <parthpatel317@gmail.com>
2017-03-20 00:30:42 -0700

    add a method to add furniture items

--- databases/awesome_build/furniture.rb
+++ databases/awesome_build/furniture.rb
@@ -8,9 +8,22 @@ db = SQLite3::Database.new("furniture.db")
 db.results_as_hash = true
 
 create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS kittens(
+  CREATE TABLE IF NOT EXISTS furniture(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    item VARCHAR(255),
     quantity INT
   )
 SQL
+
+#create a table for furniture
+db.execute(create_table_cmd)
+
+puts "Add an item! Say done when you've finished"
+item = gets.chomp
+puts "State how many of each item you want"
+quantity = gets.chomp.to_i
+
+#create a method to add many items
+def add_furniture(db, item, quantity)
+	db.execute("INSERT INTO furniture (item, quantity) VALUES (?, ?)" [item, quantity])
+end
