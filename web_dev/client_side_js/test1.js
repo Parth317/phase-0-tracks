79520a6c990d9ea54804af55982952848f830171
Parth Patel <parthpatel317@gmail.com>
2017-03-21 09:59:21 -0700

    add container and paragraph to js file, and add id = container to html file

--- web_dev/client_side_js/test1.js
+++ web_dev/client_side_js/test1.js
@@ -1,5 +1,11 @@
-console.log("Hello")
+console.log("Hello");
 
-var headers = document.getElementsByTagName('h1')
-var header = headers[0]
-header.style.fontSize = "5em"
+var headers = document.getElementsByTagName('h1');
+var header = headers[0];
+header.style.fontSize = "5em";
+
+var newParagraph = document.createElement('p');
+var paraText = document.createTextNode("This is way too large!");
+newParagraph.appendChild(paraText);
+var container = document.getElementById('container');
+container.appendChild(newParagraph);
